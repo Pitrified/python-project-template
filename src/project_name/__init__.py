152f08f5cbadda58019a6592221d120bd0fa976e
Pitrified <nobilipietro@gmail.com>
2025-12-06 23:53:08 +0100

    Enhance __init__.py to load dotenv from the new project path

--- src/project_name/__init__.py
+++ src/project_name/__init__.py
@@ -1,1 +1,10 @@
 """project_name package."""
+
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+# standard place to store credentials outside of version control and folder
+cred_path = Path.home() / "cred" / "python-project-template" / ".env"
+if cred_path.exists():
+    load_dotenv(dotenv_path=cred_path)
